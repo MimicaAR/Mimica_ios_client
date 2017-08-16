81291d734c1b2955bb45fc1b7827332cf0803e8f
Gleb Linnik <gleb_linnik@icloud.com>
2017-08-16 16:45:38 +0300

    Deleted custom navigation bar class

--- Mimica/AppDelegate.swift
+++ Mimica/AppDelegate.swift
@@ -17,8 +17,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-		FirebaseApp.configure();
+		//FirebaseApp.configure();
 		GlobalAppearanceManager();
+		
+		//ignoring the storyboard
+		window = UIWindow(frame: UIScreen.main.bounds)
+		window?.makeKeyAndVisible()
+		
+		window?.rootViewController = UINavigationController(rootViewController: HomeViewController())
+		
         return true
     }
 
--- Mimica/GlobalAppearanceManager.swift
+++ Mimica/GlobalAppearanceManager.swift
@@ -9,8 +9,27 @@
 import UIKit
 
 class GlobalAppearanceManager: NSObject {
+	private let navigationBarHeight: CGFloat = 44.0
+	
 	override init() {
 		let font: UIFont = UIFont(name: "Rubik-Medium", size: 11.0)!
 		UITabBarItem.appearance().setTitleTextAttributes([NSFontAttributeName: font], for: .normal)
+		
+		let navigationBar = UINavigationBar.appearance()
+		navigationBar.tintColor = .white
+		let gradient = CAGradientLayer()
+		gradient.frame = CGRect(x: 0, y: 0, width: UIApplication.shared.statusBarFrame.width,
+		                        height: UIApplication.shared.statusBarFrame.height + navigationBarHeight)
+		gradient.locations = [0.0, 1.0]
+		gradient.colors = [SharedStyleKit.mainGradientColor.cgColor, SharedStyleKit.mainGradientColor2.cgColor]
+		var image: UIImage? = nil
+		UIGraphicsBeginImageContext(gradient.frame.size)
+		if let context = UIGraphicsGetCurrentContext() {
+			gradient.render(in: context)
+			image = UIGraphicsGetImageFromCurrentImageContext()
+		}
+		UIGraphicsEndImageContext()
+		navigationBar.setBackgroundImage(image, for: .default)
+
 	}
 }
