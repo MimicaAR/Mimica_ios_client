52d2c1fa39fbce8e2e07a5f36ab039d4e2773626
Gleb Linnik <gleb_linnik@icloud.com>
2017-08-18 02:45:27 +0300

    Now everything without storyboard

---
+++ Mimica/GradientTabBarViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GradientTabBarViewController.swift
+//  Mimica
+//
+//  Created by Gleb Linnik on 18.08.17.
+//  Copyright © 2017 Mimica. All rights reserved.
+//
+
+import UIKit
+
+class GradientTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		setupViewControllers()
+		makeAllImagesGradient()
+    }
+	
+	private func setupViewControllers() {
+		let homeTab = UINavigationController(rootViewController: HomeViewController())
+		homeTab.tabBarItem = UITabBarItem(title: nil, image: #imageLiteral(resourceName: "Home icon"), tag: 1)
+		
+		let view2 = UIViewController()
+		view2.tabBarItem = UITabBarItem(title: nil, image: #imageLiteral(resourceName: "Play icon"), tag: 2)
+		
+		let view3 = UIViewController()
+		view3.tabBarItem = UITabBarItem(title: nil, image: #imageLiteral(resourceName: "Progress icon"), tag: 3)
+		
+		self.viewControllers = [homeTab, view2, view3]
+	}
+	
+	private func makeAllImagesGradient() {
+		for item in self.tabBar.items! {
+			item.imageInsets = UIEdgeInsets(top: 4, left: 0, bottom: -4, right: 0)
+			item.selectedImage = item.selectedImage?.tint(gradient: SharedStyleKit.mainGradient)
+			item.selectedImage = item.selectedImage?.withRenderingMode(UIImageRenderingMode.alwaysOriginal)
+		}
+	}
+}
